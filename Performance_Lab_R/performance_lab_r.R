346f033e06e830f51ebf9ed6a594d26c7a33b48e
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2021-02-24 23:35:03 -0700

    feat: Set correct working directory . . . at long last.  Love this R stuff

--- Performance_Lab_R/performance_lab_r.R
+++ Performance_Lab_R/performance_lab_r.R
@@ -1,2 +1,7 @@
+# Importing the dataset: Write code to import the dataset Boston_housing_data.csv using base R code and display the result.
 df <- read.csv("boston_housing_data.csv")
 df
+
+# Displaying the number of rows and columns:
+# Write code to display the number of rows and columns in the dataframe stored as df.
+dim(df)
