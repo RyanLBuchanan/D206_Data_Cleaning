47427f9c59fd8ab148d756f690a236219e50be5b
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2021-02-24 23:45:33 -0700

    feat: That is IT for R tonight

--- Performance_Lab_R/performance_lab_r.R
+++ Performance_Lab_R/performance_lab_r.R
@@ -5,3 +5,15 @@ df
 # Displaying the number of rows and columns:
 # Write code to display the number of rows and columns in the dataframe stored as df.
 dim(df)
+
+#Displaying the column names: Write code to display the column names in the dataframe stored as df.
+colnames(df)
+
+# Displaying mean, count, min, and max: Write code to display the mean, count, min, and max of all the numeric data using a single line of code in the dataframe stored as df.
+summary(df)
+
+#Displaying the variable type: Write code to display the structure, including each column name, its type, and its first couple of values, in the dataframe stored as df.
+str(df)
+
+# Displaying unique values: Write code to display unique values in the locationzone column in the dataframe stored as df.
+unique(df$'locationzone')
