2d93aaea2155099c70ad81ece4046336303bdc37
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2021-03-29 00:21:21 -0600

    feat: Started Lesson 6.7 PCA tutorial

--- Lesson_6/PCA_cars.R
+++ Lesson_6/PCA_cars.R
@@ -1,2 +1,10 @@
+# Load a packages (like magrittr or dplyr) that defines the function
+install.packages("magrittr") # package installations are only needed the first time you use it
+install.packages("dplyr")    # alternative installation of the %>%
+library(magrittr) # needs to be run every time you start R and want to use %>%
+library(dplyr)    # alternatively, this also loads %>%
+#The pipe operator %>% was introduced to "decrease development time and to improve readability and maintainability of code."
+
+
 mtcars[,c(1:7, 10, 11)]
 mtcars.pca <- prcomp(mtcars[,c(1:7, 10, 11)],center = TRUE, scale. = TRUE)
